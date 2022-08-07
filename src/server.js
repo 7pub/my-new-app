6eac1593264ddf289d56de522cceac338e8cbf84
7pub <office.ka24@gmail.com>
2022-08-07 08:29:04 +0200

    new file:   .gitignore 	new file:   index.js 	new file:   package-lock.json 	new file:   package.json 	new file:   preload.js 	new file:   src/7mwjIgVG.ttf 	new file:   src/7mwjJQVG.ttf 	new file:   src/7mx9IgVG.ttf 	new file:   src/7mxEIgVG.ttf 	new file:   src/7myRJQVG.ttf 	new file:   src/7myjJAVG.ttf 	new file:   src/7myjJQVG.ttf 	new file:   src/css2b431.css 	new file:   src/favicon.ico 	new file:   src/github-icon.png 	new file:   src/index.css 	new file:   src/index.html 	new file:   src/index.js 	new file:   src/index_dark.css 	new file:   src/light-bulb.png 	new file:   src/main.js 	new file:   src/more.png 	new file:   src/otp-authenticator.png 	new file:   src/package-lock.json 	new file:   src/package.json 	new file:   src/preload.js 	new file:   src/scan-image.png 	new file:   src/scan-video.png 	new file:   src/server.js 	new file:   yarn.lock

---
+++ src/server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+var path = __dirname + '/';
+var port = 5003;
+
+app.use(express.static(path));
+app.get('*', function(req, res) {
+    res.sendFile(path + '/index.html');
+});
+app.listen(port);
+//require('child_process').exec('start http://localhost:5003/');
